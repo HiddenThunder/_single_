d0be55db314adac387a5229979192550ce0e8f00
emil <poolsar.42@gmail.com>
2022-06-24 15:27:07 +0800

    starting with go routines

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"time"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
+
+func updateClock(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	clock := widget.NewLabel("")
+	w.SetContent(clock)
+
+	go func() {
+		for range time.Tick(time.Second) {
+			updateClock(clock)
+		}
+	}()
+
 	w.ShowAndRun()
 }

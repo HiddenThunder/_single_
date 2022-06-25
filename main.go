97b3d26bd3b8209da739e67ad16a471ab5d18134
emil <poolsar.42@gmail.com>
2022-06-25 18:38:40 +0800

    window size change

--- main.go
+++ main.go
@@ -3,25 +3,31 @@ package main
 import (
 	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
-func updateClock(clock *widget.Label) {
-	formatted := time.Now().Format("Time: 03:04:05")
+func updateClock(clock *widget.Label, start time.Time) {
+	diff := time.Now().Sub(start)
+	formatted := time.Time{}.Add(diff).Format("15:04:05")
 	clock.SetText(formatted)
 }
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+	start := time.Now()
 
+	a := app.New()
+	w := a.NewWindow("_maidenless_")
+
+	// banner := widget.NewLabel("no bitches")
+	w.Resize(fyne.NewSize(300, 100))
 	clock := widget.NewLabel("")
 	w.SetContent(clock)
 
 	go func() {
 		for range time.Tick(time.Second) {
-			updateClock(clock)
+			updateClock(clock, start)
 		}
 	}()
 
